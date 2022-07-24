42c8d058508265a87eb88bf21f5d6f65f4c5b00a
ValeriaMary <valeriamary.vms@gmail.com>
2022-07-24 20:15:15 -0300

    update

--- arquivos_site/site_vendas/js/alertacadastro.js
+++ arquivos_site/site_vendas/js/alertacadastro.js
@@ -1,4 +1,40 @@
-// alert ('Olá seja bem vindo ao nosso site')
 
-// var nome = prompt('Digite seu nome');
-// var idade = prompt('Digite sua idade');
+class validator{
+
+    constructor(){
+        this.validator = [
+
+        ]
+    }
+    //Iniciar validações de todos os campos
+    validate(form){
+
+        //Pegar os inputs
+        let inputs = form.getElementsByTagName('input');
+
+        //Transformo uma HTMLCollection -> Array
+        let inputsArray = [...inputs]
+
+        //Loop nos inputs  e validações  mediante  ao que for enconrtrado
+        inputsArray.forEach(function(input) {
+
+        });
+    }
+}
+
+
+
+
+let form = document.getElementById("register-form");
+let submit = document.getElementById("btn-submit")
+
+//Evento que dispara as validações
+
+submit.addEventListener('click',function (e){
+    
+    e.preventDefault(); 
+
+    console.log('Funcionou');
+
+});
+
